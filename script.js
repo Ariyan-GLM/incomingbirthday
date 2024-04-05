b888c3b4950020c7ef55f9b841da9161b83b68d0
ARITRA BISWAS <166093066+Ariyan-GLM@users.noreply.github.com>
2024-04-05 18:04:06 +0530

    added script.js

---
+++ script.js
@@ -0,0 +1,28 @@
+alert("Waiting for your Birthday.")
+
+const daysEl = document.getElementById("days")
+const hoursEl = document.getElementById("hours")
+const minsEl = document.getElementById("mins")
+const secEl =  document.getElementById("sec")
+
+const birthday = '06 april 2024';
+
+function countdown(){
+    const birthDayDate = new Date(birthday);
+    const currentDate = new Date();
+    const totalSeconds = (birthDayDate - currentDate) /1000;
+
+    const days = Math.floor(totalSeconds / 3600 / 24);
+    const hours = Math.floor(totalSeconds / 3600 ) % 24;
+    const mins = Math.floor(totalSeconds / 60 ) % 60;
+    const seconds = Math.floor(totalSeconds) % 60;
+
+    // console.log (days, hours, mints, seconds);
+    daysEl.innerHTML = days;
+    hoursEl.innerHTML = hours;
+    minsEl.innerHTML = mins;
+    secEl.innerHTML = seconds;
+}
+
+countdown();
+setInterval(countdown, 1000);
